c78242733494ce1b28163121488e6999e5fbb321
Daniel Xie <xiedaniel@outlook.com>
2019-09-21 20:10:02 +0800

    改进了两个问题的算法

--- question12.js
+++ question12.js
@@ -1,3 +1,19 @@
 function Power(base, exponent) {
-  return Math.pow(base, exponent);
+  let result = 1;
+  if(exponent > 0) {
+    for(let i = 1; i <= exponent; i++) {
+      result *= base;
+    }
+  } else if(exponent < 0) {
+    if(base == 0) {
+      return Infinity;
+    } else {
+      exponent = -exponent;
+      for(let i = 1; i <= exponent; i++) {
+        result *= base;
+      }
+      result = 1 / result;
+    }
+  }
+  return result;  
 }
