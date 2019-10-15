1cb80b850f11066bf9358e6c4259153a0ec0b254
Daniel Xie <xiedaniel@outlook.com>
2019-10-15 22:26:05 +0800

    删除了算法中部分多余的代码

--- question55.js
+++ question55.js
@@ -1,11 +1,12 @@
-function ListNode(x){
+/*function ListNode(x){
     this.val = x;
     this.next = null;
-}
+}*/
 function EntryNodeOfLoop(pHead) {
   if(!pHead) {
     return null;
   }
+
   let p1 = pHead, p2 = pHead;
   do {
     if(p1.next && p2.next && p2.next.next) {
@@ -14,11 +15,7 @@ function EntryNodeOfLoop(pHead) {
     } else {
       return null;
     }
-  } while(p1 != p2 && p1 != null && p2 != null)
-
-  if(p1 == null || p2 == null) {
-    return null;
-  }
+  } while(p1 != p2 && p1 != null && p2 != null);
 
   let count = 1;
   p1 = p1.next;
@@ -28,7 +25,6 @@ function EntryNodeOfLoop(pHead) {
   }
 
   p1 = p2 = pHead;
-
   while(count > 0) {
     p1 = p1.next;
     count--;
@@ -38,6 +34,5 @@ function EntryNodeOfLoop(pHead) {
     p1 = p1.next;
     p2 = p2.next;
   }
-
   return p1;
 }
